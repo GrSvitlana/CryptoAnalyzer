5cf99a4ceab6b5d9dff32bb0422fa0daad463fb6
Svitlana Graf <svetlanapss@gmail.com>
2022-07-17 21:17:16 +0200

    added BruteForce class

--- src/main/java/org/firstproject/graf/cryptoanalyzer/constant/Constants.java
+++ src/main/java/org/firstproject/graf/cryptoanalyzer/constant/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 	//private static final String latin = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	private static final String cyrillic = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
 	private static final String number = "0123456789";
-	private static final String symbol = "§!@#$%ˆ&*/+-()_={}[]|<>?. ,";
+	private static final String symbol = "§!@#$%ˆ&*/+-()_={}[]|<>?. ,\n";
 	//public static final String ALPHABET = latin + latin.toLowerCase() + cyrillic + cyrillic.toLowerCase() + number + symbol;
 	public static final String ALPHABET = cyrillic + cyrillic.toLowerCase() + number + symbol;
 	
